a5de5586367ece82aec3b3c0058965416c17b1a3
Richard Herveille <richard.herveille@roalogic.com>
2024-01-13 08:26:37 +0100

    Initial read test

--- bench/verilog/tst_tests.sv
+++ bench/verilog/tst_tests.sv
@@ -11,7 +11,7 @@
 //                                                                //
 ////////////////////////////////////////////////////////////////////
 //                                                                //
-//     Copyright (C) 2023 ROA Logic BV                            //
+//     Copyright (C) 2023-2024 ROA Logic BV                       //
 //     www.roalogic.com                                           //
 //                                                                //
 //     This source file may be used and distributed without       //
@@ -41,7 +41,7 @@
  */
 localparam int AHB_ADDRESS_BOUNDARY = 1024;
 
-//`define __DEBUG
+`define __DEBUG
 
 /* Error counter
  */
@@ -83,6 +83,10 @@ task test_buffer_fill_random;
   foreach (test_buffer[i])
     test_buffer[i] = $urandom;
 endtask : test_buffer_fill_random
+
+
+//SDRAM tasks in tst_sdram_lowlvl
+
 
 
 /* Useful functions
@@ -173,53 +177,53 @@ task automatic write_sdram_seq(
 endtask : write_sdram_seq
 
 
-  task automatic read_sdram_seq(
-    input [HSIZE_SIZE-1:0 ] size,
-    input [HBURST_SIZE-1:0] burst,
-    input int               runs
-  );
-    //Reading sdram sequentially
-    //start at address 0
-    int                                 run = 0;
-    int                                 burstsize;
-    logic        [SDRAM_ADDR_SIZE -1:0] adr;
-    static logic [HDATA_SIZE-1:0]       rbuf[];
+task automatic read_sdram_seq(
+  input [HSIZE_SIZE-1:0 ] size,
+  input [HBURST_SIZE-1:0] burst,
+  input int               runs
+);
+  //Reading sdram sequentially
+  //start at address 0
+  int                                 run = 0;
+  int                                 burstsize;
+  logic        [SDRAM_ADDR_SIZE -1:0] adr;
+  static logic [HDATA_SIZE-1:0]       rbuf[];
 
-    //create read buffer
-    burstsize = hburst2int(burst);
-    rbuf = new[burstsize];
+  //create read buffer
+  burstsize = hburst2int(burst);
+  rbuf = new[burstsize];
+
+  //start at address 0
+  adr = 0;
+
+  while (run < runs)
+  begin
+      //AHB read
+      ahb_if[AHB_CTRL_PORT].ahb_bfm.read(adr, rbuf, size, burst);
+
+      //next address
+      adr = adr + (burstsize << size);
+
+      //next run
+      run++;
+  end
+
+  //idle AHB bus
+  ahb_if[AHB_CTRL_PORT].ahb_bfm.idle();
+
+  wait fork;
+endtask : read_sdram_seq
 
 
-    //start at address 0
-    adr = 0;
-
-    while (run < runs)
-    begin
-        //AHB read
-        ahb_if[AHB_CTRL_PORT].ahb_bfm.read(adr, rbuf, size, burst);
-
-        //next address
-        adr = adr + (burstsize << size);
-
-        //next run
-        run++;
-    end
-
-    //idle AHB bus
-    ahb_if[AHB_CTRL_PORT].ahb_bfm.idle();
-
-    wait fork;
-  endtask : read_sdram_seq
-
-
-//Reads content from test_buffer and writes it to the SDRAM
-task tst_write (
+/* -- Write
+ * Reads content from test_buffer and writes it to the SDRAM
+ */
+task automatic tst_write (
     input int               port,
     input [HADDR_SIZE -1:0] start_address,
     input [HSIZE_SIZE -1:0] hsize,
     input [HBURST_SIZE-1:0] hburst
 );
-
   static logic [HDATA_SIZE-1:0] buffer[];
   logic                  wrap_burst;
   int                    wrap_size;
@@ -249,12 +253,89 @@ task tst_write (
     begin
         offset = wrap_burst ? (start_address & ~adr_mask) | ((start_address + (i << hsize) +b) & adr_mask)
                             : start_address + (i << hsize) +b;
-        buffer[i][b*8 +: 8] = test_buffer[offset / (SDRAM_DQ_SIZE/8)][ (offset % (SDRAM_DQ_SIZE/8)) *8 +: 8];
+        buffer[i][b*8 +: 8] = test_buffer[offset / (SDRAM_DQ_SIZE/8)][(offset % (SDRAM_DQ_SIZE/8)) *8 +: 8];
     end
 
   //AHB write
   ahb_if[AHB_CTRL_PORT].ahb_bfm.write(start_address, buffer, hsize, hburst);
 endtask : tst_write
+
+
+/* -- Read
+ * Reads content from sdram and writes it to the testbuffer
+ */
+task automatic tst_read (
+    input int               port,
+    input [HADDR_SIZE -1:0] start_address,
+    input [HSIZE_SIZE -1:0] hsize,
+    input [HBURST_SIZE-1:0] hburst
+);
+  logic [HDATA_SIZE-1:0] buffer[];
+  logic                  wrap_burst;
+  int                    wrap_size;
+  logic [HADDR_SIZE-1:0] adr_mask;
+  int                    offset;
+  int                    beats;
+
+static int transaction=0;
+
+  //Is this a wrapping burst?
+  wrap_burst = ~hburst[0];
+
+  //How many beats in a transaction
+  beats = hburst2int(hburst);
+
+  //calculate address mask
+  if (wrap_burst) //strictly not necessary, but speeds up sims
+  begin
+      wrap_size = beats << hsize;
+      adr_mask  = wrap_size -1;
+  end
+
+  //create new transaction buffer
+//  buffer = new[beats];
+
+  fork
+     //AHB read
+     ahb_if[AHB_CTRL_PORT].ahb_bfm.read(start_address, buffer, hsize, hburst);
+
+     begin
+         //prepare address phase
+$display ("1: @%0t", $realtime);
+         do
+           @(posedge HCLK);
+         while (!HREADY[AHB_CTRL_PORT]);
+
+         //wait for address phase (1st address is presented here)
+$display ("2: @%0t", $realtime);
+         do
+           @(posedge HCLK);
+         while (!HREADY[AHB_CTRL_PORT]);
+$display ("3: @%0t", $realtime);
+
+         //Data phase(s)
+         for (int i=0; i < beats; i++)
+         begin
+$display("4: @%0t", $realtime);
+             do
+               @(posedge HCLK);
+             while (!HREADY[AHB_CTRL_PORT]);
+$display("5: @%0t", $realtime);
+
+             for (int b=0; b < hsize2bytes(hsize); b++)
+             begin
+                 offset = wrap_burst ? (start_address & ~adr_mask) | ((start_address + (i << hsize) +b) & adr_mask)
+                                     : start_address + (i << hsize) +b;
+                 test_buffer[offset / (SDRAM_DQ_SIZE/8)][(offset % (SDRAM_DQ_SIZE/8)) *8 +: 8] = HRDATA[AHB_CTRL_PORT][(offset % (HDATA_SIZE/8)) *8 +: 8];
+             end
+
+`ifdef __DEBUG
+             $display("test_buffer[%0h]:%0h, HRDATA=%h @%0t", offset/(SDRAM_DQ_SIZE/8), test_buffer[offset / (SDRAM_DQ_SIZE/8)], HRDATA[AHB_CTRL_PORT], $realtime);
+`endif
+         end
+     end
+  join_any
+endtask : tst_read
 
 
 /* -- Compare
@@ -283,7 +364,7 @@ task tst_compare (
       total++;
       if ( test_buffer_value !== sdram_value )
       begin
-          $display ("ERROR, compare mismatch for HADDR=%0h. Expected %2h, received %2h @%0t", adr, test_buffer_value, sdram_value, $realtime);
+          $display ("ERROR, compare mismatch for HADDR=%0h. Test_buffer=%2h, SDRAM=%2h @%0t", adr, test_buffer_value, sdram_value, $realtime);
           bad++; ugly++;
       end
       else
@@ -311,7 +392,7 @@ task tst_write_sequential (
 
   $display ("\n");
   $display ("-------------------------------------------------------------");
-  $display (" tst_write_sequential started (%0s)", random ? "random data" : "sequential data");
+  $display (" tst_write_sequential started (%0s) @%0t", random ? "random data" : "sequential data", $realtime);
   $display ("-------------------------------------------------------------");
 
   if (random) test_buffer_fill_random();
@@ -338,10 +419,10 @@ task tst_write_sequential (
       for (int adr=0; adr < runs; adr++)
       begin
           //skip transactions that cross the AHB address boundary
-          if (!wrap_burst &&
+/*          if (!wrap_burst &&
               ((adr & (AHB_ADDRESS_BOUNDARY-1)) + (hburst2int(hburst) << hsize) > AHB_ADDRESS_BOUNDARY)
              ) continue;
-
+*/
           //perform AHB write
           tst_write(AHB_CTRL_PORT, adr, hsize, hburst);
 
@@ -362,7 +443,7 @@ task tst_write_sequential (
   end
 
   $display ("-------------------------------------------------------------");
-  $display (" tst_write_sequential finished");
+  $display (" tst_write_sequential finished @%0t", $realtime);
   $display (" Total tests: %0d, failed tests: %0d", total-total_start, ugly-ugly_start);
   $display ("-------------------------------------------------------------");
 
@@ -391,7 +472,7 @@ task tst_write_random (
 
   $display ("\n");
   $display ("-------------------------------------------------------------");
-  $display (" tst_write_random started");
+  $display (" tst_write_random started @%0t", $realtime);
   $display ("-------------------------------------------------------------");
 
   //clear test_buffer
@@ -424,15 +505,10 @@ task tst_write_random (
       beats = hburst2int(hburst);
 
       //calculate address mask
-      wrap_size = beats << hsize;
       if (wrap_burst) //strictly not required, but speeds up sims
       begin
+          wrap_size = beats << hsize;
           adr_mask  = wrap_size -1;
-      end
-      else
-      begin
-          //skip transactions that cross the AHB address boundary
-          if (((haddr & (AHB_ADDRESS_BOUNDARY-1)) + wrap_size) > AHB_ADDRESS_BOUNDARY) continue;
       end
 
       //write values to testbuffer
@@ -471,8 +547,77 @@ task tst_write_random (
   tst_compare(0, haddr_range);
 
   $display ("-------------------------------------------------------------");
-  $display (" tst_write_random finished");
+  $display (" tst_write_random finished @%0t", $realtime);
   $display (" Total tests: %0d, failed tests: %0d", total-total_start, ugly-ugly_start);
   $display ("-------------------------------------------------------------");
 
 endtask : tst_write_random
+
+
+// read sequential data
+task tst_read_sequential (
+  input int runs,
+  input bit random
+);
+  logic wrap_burst;
+  int hsize;
+  int hburst;
+
+  int total_start;
+  int ugly_start;
+
+  $display ("\n");
+  $display ("-------------------------------------------------------------");
+  $display (" tst_read_sequential started (%0s) @%0t", random ? "random data" : "sequential data", $realtime);
+  $display ("-------------------------------------------------------------");
+
+  if (random) sdram_fill_random();
+  else        sdram_fill_sequential();
+
+  total_start = total;
+  ugly_start  = ugly;
+
+  hsize  = 3'b000;
+  hburst = 3'b011;
+  for (hsize  = 0; hsize  < $clog2(HDATA_SIZE/8); hsize++)
+  for (hburst = 0; hburst < 8;                    hburst++)
+  begin
+      if (hburst == HBURST_INCR) hburst++;
+
+      wrap_burst = ~hburst[0];
+
+      test_buffer_clear();
+      clear_error_counters();
+
+      $display ("tst_read_sequential; hsize=%3b, hburst=%3b", hsize, hburst);
+
+      //perform writes
+      for (int adr=0; adr < runs; adr++)
+      begin
+          //perform AHB read
+          tst_read(AHB_CTRL_PORT, adr, hsize, hburst);
+
+          //show test progress
+          if (adr % 500_000 == 0) $display("  run:%0d of %0d (%0d%%)", adr, runs, 100*adr/runs);
+      end
+
+      //idle AHB bus
+      ahb_if[AHB_CTRL_PORT].ahb_bfm.idle();
+
+      //wait for HREADY
+      do
+        @(posedge HCLK);
+      while (!HREADY[AHB_CTRL_PORT]);
+
+      //check results
+      tst_compare(0, runs);
+
+      $display(" --Done. Good=%0d, Bad=%0d, Ugly=%0d", good, bad, ugly);
+  end
+
+  $display ("-------------------------------------------------------------");
+  $display (" tst_read_sequential finished");
+  $display (" Total tests: %0d, failed tests: %0d", total-total_start, ugly-ugly_start);
+  $display ("-------------------------------------------------------------");
+
+endtask : tst_read_sequential
