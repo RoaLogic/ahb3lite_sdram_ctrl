033e30680a07c8d4be5f40fa5d6399a57d53faab
Richard Herveille <richard.herveille@roalogic.com>
2024-05-18 18:22:52 +0200

    Fixed rdq size

--- rtl/verilog/ahb3lite_sdram_ctrl.sv
+++ rtl/verilog/ahb3lite_sdram_ctrl.sv
@@ -184,7 +184,7 @@ module ahb3lite_sdram_ctrl
   logic [MAX_RSIZE          -1:0] rdrow     [AHB_PORTS];
   logic [MAX_CSIZE          -1:0] rdcol     [AHB_PORTS];
   logic [                    7:0] rdsize    [AHB_PORTS];
-  logic [HDATA_SIZE         -1:0] rdq       [AHB_PORTS];
+  logic [SDRAM_DQ_SIZE      -1:0] rdq       [AHB_PORTS];
   logic                           rdqvalid  [AHB_PORTS];
 
   logic                           wrreq     [AHB_PORTS];
